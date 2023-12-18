743cac38b8bf5aea2b07e63153e5ebd8ffd8abef
danielepaula93 <contato.danielepaula@outlook.com>
2023-12-18 17:56:18 -0300

    ajuste da listagem de users

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -12,14 +12,6 @@ namespace Biblioteca.Controllers
 {
     public class UsuarioController : Controller
     {
-        /*
-        public IActionResult ListaDeUsuarios() {
-            Autenticacao.CheckLogin(this);
-            Autenticacao.verificaSeUsuarioEAdmin(this);
-
-            return View(new UsuarioService().Listar());
-        }
-        */
 
         public IActionResult Cadastro() {
             Autenticacao.CheckLogin(this);
@@ -71,74 +63,41 @@ namespace Biblioteca.Controllers
             Usuario usuario = usuarioService.ObterPorId(id);
             return View(usuario);
         }
+/*
+        public IActionResult Excluir() {
+            Autenticacao.CheckLogin(this);
+            UsuarioService usuarioService = new UsuarioService();
+            Usuario usuario = usuarioService.ObterPorId(id);
+            return View("Excluir");
+        }
 
         [HttpPost]
-        public IActionResult Exclui(int id) {
+        public IActionResult Excluir(int id) {
             Autenticacao.CheckLogin(this);
             UsuarioService usuarioService = new UsuarioService();
             usuarioService.Excluir(id);
             return RedirectToAction("Listagem");
         }
-/*
-        public IActionResult RegistrarUsuarios(int id) {
-            
-            Autenticacao.CheckLogin(this);
-            Autenticacao.verificaSeUsuarioEAdmin(this);
-            return View();
-
-        }
-
-        public IActionResult RegistrarUsuarios(Usuario novoUser) {
-            
-            Autenticacao.CheckLogin(this);
-            Autenticacao.verificaSeUsuarioEAdmin(this);
-            
-            novoUser.Senha = Criptografo.TextoCriptografado(novoUser.Senha);
-
-            UsuarioService us = new UsuarioService();
-            us.incluirUsuario(novoUser);
-
-            return RedirectToAction("cadastroRealizado");
-
-        }
-
-        public IActionResult excluirUsuario(int id) {
-
-            return View(new UsuarioService().Listar(id));
-
-        }
-
-        [HttpPost]
-        public IActionResult ExcluirUsuario(string decisao, int id) {
-
-            if(decisao == "EXCUIR") {
-                ViewData["Mensagem"] = "Exclusão de Usuário " + new UsuarioService().Listar(id).Nome + "realizada com sucesso";
-                new UsuarioService().excluirUsuario(id);
-                return View("ListaDeUsuarios", new UsuarioService().Listar());
-            } else {
-                ViewData["Mensagem"] = "Exclusão de Cancelada";
-                return View("ListaDeUsuarios", new UsuarioService().Listar());
-            }
-
-        }
-
-        public IActionResult cadastroRealizado() {
-            Autenticacao.CheckLogin(this);
-            Autenticacao.verificaSeUsuarioEAdmin(this);
-            return View();
-        }
-
-        public IActionResult NeedAdmin() {
-            Autenticacao.CheckLogin(this);
-            return View();
-        }
-
-        public IActionResult Sair() {
-            HttpContext.Session.Clear();
-            return RedirectToAction("Index", "Home");
-        }
-
         */
+
+        public IActionResult Excluir(int id)
+    {
+        UsuarioService usuarioService = new UsuarioService();
+        Usuario usuario = usuarioService.ObterPorId(id);
+
+        return View(usuario);
+    }
+
+    [HttpPost]
+    public IActionResult ConfirmarExclusao(int id)
+    {
+        UsuarioService usuarioService = new UsuarioService();
+        Usuario usuario = usuarioService.ObterPorId(id);
+
+        usuarioService.Excluir(id);
+
+        return RedirectToAction("Listagem"); // Redirecione para a página principal ou outra página desejada
+    }
 
     }
 }
